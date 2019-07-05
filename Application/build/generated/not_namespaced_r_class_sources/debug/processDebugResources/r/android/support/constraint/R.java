7a75c2897e6340966771e0e9918b0426cec303b0
Samira <samira.huber@freenet.de>
2019-07-05 06:36:37 +0200

    add settings layout and set color in colors.xml

--- Application/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ Application/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -75,18 +75,18 @@ public final class R {
 
         public static final int bottom = 0x7f070021;
         public static final int end = 0x7f070035;
-        public static final int gone = 0x7f07003b;
-        public static final int invisible = 0x7f070048;
-        public static final int left = 0x7f07004a;
-        public static final int packed = 0x7f07005a;
-        public static final int parent = 0x7f07005b;
-        public static final int percent = 0x7f07005d;
-        public static final int right = 0x7f070061;
-        public static final int spread = 0x7f07007a;
-        public static final int spread_inside = 0x7f07007b;
-        public static final int start = 0x7f070080;
-        public static final int top = 0x7f07008f;
-        public static final int wrap = 0x7f070095;
+        public static final int gone = 0x7f07003c;
+        public static final int invisible = 0x7f070049;
+        public static final int left = 0x7f07004b;
+        public static final int packed = 0x7f07005b;
+        public static final int parent = 0x7f07005c;
+        public static final int percent = 0x7f07005e;
+        public static final int right = 0x7f070065;
+        public static final int spread = 0x7f07007e;
+        public static final int spread_inside = 0x7f07007f;
+        public static final int start = 0x7f070084;
+        public static final int top = 0x7f070093;
+        public static final int wrap = 0x7f070099;
     }
     public static final class styleable {
         private styleable() {}
